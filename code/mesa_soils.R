e26f3df2592a5fcd8c662e0db67bd906fc5d2e8c
fjoyce <fhjoyce@gmail.com>
2025-08-01 16:16:02 -0700

    some wrangling, mostly plotting

--- code/mesa_soils.R
+++ code/mesa_soils.R
@@ -21,7 +21,8 @@ upper_long <- upper %>%
     port %in% c("port1_vwc", "port3_vwc", "port5_vwc") ~ "shallow",
     port %in% c("port2_vwc", "port4_vwc", "port6_vwc") ~ "deep",
     .default = NA
-  ))
+  )) %>% 
+  mutate(depth = fct_relevel(depth, "shallow", "deep"))
 
 ## lower array data ----
 # load in lower VWC sensor data
@@ -50,8 +51,8 @@ vwc_scale <- scale_y_continuous(limits = c(0,0.7))
 
 
 #plot1 with upper lable and colored by depth
-plot_upper1 <- ggplot(upper_long, aes(x=timestamp, y=vwc, col=depth, group=port)) + 
-  geom_line() +
+plot_upper1 <- ggplot(upper_long, aes(x=timestamp, y=vwc, col=depth)) + 
+  geom_line(aes(group = port)) +
   ggtitle("Upper") +
   scale_x_datetime(date_breaks = "2 month",
                    date_labels = "%b") +
@@ -71,8 +72,8 @@ plot_upper2 <- ggplot(upper_long, aes(x=timestamp, y=vwc, col=port)) +
 plot_upper2
 
 # lower plots 
-plot_lower1 <- ggplot(lower_long, aes(x=timestamp, y=vwc, col=depth, group=port)) + 
-  geom_line() +
+plot_lower1 <- ggplot(lower_long, aes(x=timestamp, y=vwc, col=depth)) + 
+  geom_line(aes(group = port)) +
   ggtitle("Lower") +
   scale_x_datetime(date_breaks = "2 month",
                    date_labels = "%b") +
@@ -125,7 +126,17 @@ mesa_gwc_filter <- mesa_gwc %>%
   #filter to just M1 & M2 sample points
   filter(site == "M" & sample <3) %>% 
   filter(depth_in %in% c("6", "18")) %>% 
-  unite("sample_point", site, sample, sep = "_", remove = TRUE)
+  unite("sample_point", site, sample, sep = "_", remove = TRUE) %>% 
+  mutate(
+    slope_position = case_when(
+      sample_point == "M_1" ~ "upper",
+      sample_point == "M_2" ~ "lower"),
+    depth = case_when(
+      depth_in == "6" ~ "shallow",
+      depth_in == "18" ~ "deep"
+    )
+    
+    )
 
 #plot over time
 
@@ -142,7 +153,7 @@ fig_gwc
   
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ ----
 
-#Combine
+#Combine data sets ----
 
 #get list of gwc sampling dates
 sample_dates <- unique(mesa_gwc_filter$sample_date)
@@ -164,12 +175,78 @@ lower_vwc_filt <- lower_long %>%
   ungroup() %>% 
   mutate(slope_position = "lower")
 
-vwc_filt_combined <- rbind(upper_vwc_filt, lower_vwc_filt)
+vwc_filt_combined <- rbind(upper_vwc_filt, lower_vwc_filt) %>% 
+  #group by date, array, and depth
+  group_by(date, slope_position, depth) %>% 
+  summarize(vwc = mean(mean_vwc)) %>% 
+  ungroup()
 
+wc_combined <- mesa_gwc_filter %>%
+  left_join(vwc_filt_combined, by =join_by(slope_position, depth, sample_date == date)) %>% 
+  unite("point", slope_position, depth, sep = "_", remove = FALSE) %>% 
+  mutate(timestamp = as.POSIXct(sample_date)) %>% 
+  mutate(depth = fct_relevel(depth, "shallow", "deep")) %>% 
+  mutate(slope_position = fct_relevel(slope_position, "upper", "lower"))
 
+# scatterplot ----
+fig_wc_comparison <- ggplot(data = wc_combined, aes(x = soil_moisture, y = vwc, color = depth)) +
+  geom_point() +
+  xlab("Gravimetric water content") +
+  ylab("Volumetric water content") + 
+  geom_abline(slope = 1, intercept = 0) +
+  theme_cowplot() +
+  scale_x_continuous(limits = c(0,NA), expand = c(0,NA)) +
+  scale_y_continuous(limits = c(0,NA),expand = c(0,NA)) +
+  facet_wrap(vars(slope_position), nrow = 1) +
+  coord_fixed()
+ 
+fig_wc_comparison
 
+ggsave("figures/mesa_slope_wc_scatterplot_2025-08-01.pdf")
 
+#wc time series ----
 
+gwc_upper <- wc_combined %>% 
+  filter(slope_position == "upper")
 
+gwc_lower <- wc_combined %>% 
+  filter(slope_position == "lower")
+
+fig_upper_combined <- plot_upper1 +
+  #add dotted lines for sample dates
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-02-28")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-04-07")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-04-23")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-05-07")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-06-01")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-07-17")), linetype = "dotted") +
+  geom_point(data = gwc_upper, 
+             aes(x = timestamp, y = soil_moisture, fill = depth)) +
+  ylab("Water content") +
+  xlab("Date")
+
+fig_upper_combined 
+
+fig_lower_combined <- plot_lower1 +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-02-28")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-04-07")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-04-23")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-05-07")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-06-01")), linetype = "dotted") +
+  geom_vline(xintercept = as.numeric(as.POSIXct("2025-07-17")), linetype = "dotted") +
+  geom_point(data = gwc_lower, 
+             aes(x = timestamp, y = soil_moisture, fill = depth)) +
+  ylab("Water content") +
+  xlab("Date")
+  
+fig_lower_combined
+
+fig_combined <- plot_grid(fig_upper_combined, fig_lower_combined,
+                          nrow = 1)
+
+fig_combined
+
+ggsave("figures/mesa_slope_water_content_comparison_2025-08-01.pdf",
+       fig_combined)
 
 
