f079f6054810f3402155dea6088a35cfafac4ab6
fjoyce <fhjoyce@ucsb.edu>
2025-12-06 15:02:12 -0800

    revise code to read in December (or Nov) Excel files, add notes on data gaps, filter out data when timestamp was not correct, make new figure with just two panels

--- code/01_volumetric_water_content.R
+++ code/01_volumetric_water_content.R
@@ -1,17 +1,24 @@
 # set up ----
 source("code/00_setup.R")
 
+library(scales)
 
 #Volumetric water content ----
 
 ## upper array ----
 # load in upper VWC sensor data
-upper <- read_csv("data/volumetric_water_content/z6B01375 27Jul25-1901upperall.csv", na = c("NA", "", "#N/A")) %>% 
+
+upper <- readxl::read_xlsx("data/volumetric_water_content/raw_sensor_data/z6B01375 11Nov25-1146_NO_ROWS_SINCE_9-9-25.xlsx", 
+                           sheet = "Processed Data Config 1",
+                           col_names = c("Timestamp",	"Port1_VWC",	"Port2_VWC",	"Port3_VWC",	"Port4_VWC",	"Port5_VWC",	"Port6_VWC",	"Bat_percent",	"mV.Battery.Voltage"),
+                           col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), 
+                           skip=3) %>%
   #clean up names
   clean_names() %>% 
-  #reformat timestamp as a proper date-time
-  mutate(timestamp = mdy_hm(timestamp))
-
+  #Looks like the battery came back to life briefly after 2025-09-09, but date was reset...
+  #filter to data after Jan 1 2025
+  filter(timestamp > as.POSIXct("2025-01-01"))
+                
 upper_long <- upper %>% 
   #remove battery columns
   select(-bat_percent,-m_v_battery_voltage) %>% 
@@ -25,13 +32,20 @@ upper_long <- upper %>%
   )) %>% 
   mutate(depth = fct_relevel(depth, "shallow", "deep"))
 
+
 ## lower array ----
 # load in lower VWC sensor data
-lower <- read_csv("data/volumetric_water_content/z6B01383 27Jul25-1904lowerall.csv", na = c("NA", "", "#N/A")) %>% 
-  #clean up names
+
+#note, seems data logger reverted to default time sometime after 2025-10-29 02:00:00
+#time was reset on "2025-11-07 14:00:00"?
+
+lower <- readxl::read_xlsx("data/volumetric_water_content/raw_sensor_data/z6B01383 05Dec25-1318_Lower_All_Rows.xlsx", 
+                           sheet = "Processed Data Config 1",
+                           col_names = c("Timestamp",	"Port1_VWC",	"Port2_VWC",	"Port3_VWC",	"Port4_VWC",	"Port5_VWC",	"Port6_VWC",	"Bat_percent",	"mV.Battery.Voltage"),
+                           col_types = c("date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), 
+                           skip=3) %>% 
   clean_names() %>% 
-  #reformat timestamp as a proper date-time
-  mutate(timestamp = mdy_hm(timestamp))
+  filter(timestamp > as.POSIXct("2025-01-01"))
 
 lower_long <- lower %>% 
   #remove battery columns
@@ -43,7 +57,8 @@ lower_long <- lower %>%
     port %in% c("port1_vwc", "port3_vwc", "port5_vwc") ~ "shallow",
     port %in% c("port2_vwc", "port4_vwc", "port6_vwc") ~ "deep",
     .default = NA
-  ))
+  )) %>% 
+  mutate(depth = fct_relevel(depth, "shallow", "deep"))
 
 ## visualize VWC ----
 
@@ -77,7 +92,7 @@ plot_lower1 <- ggplot(lower_long, aes(x=timestamp, y=vwc, col=depth)) +
   geom_line(aes(group = port)) +
   ggtitle("Lower") +
   scale_x_datetime(date_breaks = "2 month",
-                   date_labels = "%b") +
+                   date_labels = "%b") + 
   LA_theme +
   vwc_scale
 
@@ -96,8 +111,25 @@ plot_lower2
 fig_vwc <- plot_grid(plot_upper1, plot_lower1, plot_upper2, plot_lower2,
                      nrow = 2) 
 
-
 fig_vwc
 
-ggsave("figures/Mesa_Slope_VWC_2025-07-27.pdf",
-       fig_vwc)
+#rename output file
+#ggsave("figures/Mesa_Slope_VWC_2025-12-06.pdf",
+#       fig_vwc)
+
+
+# two-panel version
+
+date_scale <- scale_x_datetime(date_breaks = "1 month", 
+                               date_labels = "%b",
+                               limits = c(as.POSIXct("2025-01-15"), as.POSIXct("2025-12-15")))
+
+fig_vwc_2_panel <- plot_grid(plot_upper1 + date_scale + xlab("Timestamp") + ylab("VWC"), 
+                             plot_lower1 + date_scale + xlab("Timestamp") + ylab("VWC"),
+                             nrow = 2,
+                             align = "v")
+
+fig_vwc_2_panel
+
+
+ggsave("figures/vwc/Mesa_Slope_VWC_2025-12-06.pdf")
