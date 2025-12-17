e5c1e52344e5ab766b2c7f44e04eb77ce171b54b
emileedoering4 <emilee.doering@gmail.com>
2025-12-17 13:52:39 -0800

    Add scripts to plot water potential from December and Combined November/December

---
+++ code/CombinedWP_12:3_11:7_Script.R
@@ -0,0 +1,95 @@
+## Script for combined graph of November and December water potentials for MU, ML, and SP reference sites ##
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(cowplot)
+library(scales)
+
+## Process data sets ##
+process_wp <- function(file_path, date_label) {
+  wp <- read_csv(file_path) %>%
+    separate_wider_delim(
+      Sp_sample,
+      delim = "_",
+      names = c("species", "sample")
+    ) %>%
+    mutate(
+      species = case_when(
+        species == "Bapi" ~ "BAPI",
+        species == "Arca" ~ "ARCA",
+        TRUE ~ species
+      ),
+      date = date_label
+    ) %>%
+    group_by(species, Location, sample, date) %>%
+    summarize(
+      mean_MD = mean(MPa, na.rm = TRUE),
+      n = n(),
+      .groups = "drop"
+    )
+  return(wp)
+}
+
+### Select and Process data sets ##
+
+wp_2025_12_03 <- process_wp(
+  "/Users/emileedoering/Desktop/NCOS_Water_Potentials  copy/NewWater_potentials_2025-12-03 2.csv",
+  "2025-12-03"
+)
+
+wp_2025_11_07 <- process_wp(
+  "/Users/emileedoering/Desktop/NCOS_Water_Potentials  copy/Water Potentials 11_7 - Sheet1.csv",
+  "2025-11-07"
+)
+
+## Combine data sets ## 
+
+wp_combined <- bind_rows(wp_2025_11_07, wp_2025_12_03)
+
+## Plot side by side ##
+
+wp_combined_fig <- ggplot(
+  wp_combined,
+  aes(
+    x = Location,
+    y = mean_MD,
+    color = date,               
+    shape = species,           
+    group = interaction(species, date)  
+  )
+) +
+  geom_jitter(
+    position = position_dodge(width = 0.6),
+    size = 2
+  ) +
+  facet_wrap(vars(species)) +
+  theme_cowplot() +
+  scale_y_continuous(
+    limits = c(0, NA),
+    breaks = breaks_width(0.5)
+  ) +
+  labs(
+    y = "Water potential (-MPa)",
+    x = "Location",
+    color = "Date",
+    shape = "Species"
+  ) +
+  theme(axis.text.x = element_text(angle = 360, hjust = 1))
+
+## Display plot ##
+
+wp_combined_fig
+
+## Save to folder ##
+
+ggsave(
+  wp_combined_fig,
+  filename = "/Users/emileedoering/Desktop/NCOS_Water_Potentials  copy/waterpotentials_11_7_vs_12_03_sidebyside.png",
+  width = 220,
+  height = 120,
+  units = "mm",
+  bg = "white"
+)
+
