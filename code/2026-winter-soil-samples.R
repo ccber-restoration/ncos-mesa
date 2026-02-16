0c8c0608f1a658b2993418ff44dc2bfacdf0d0e7
fjoyce <fhjoyce@ucsb.edu>
2026-02-16 14:37:00 -0800

    update to most recent data, code organization, add mean line to figures

--- code/2026-winter-soil-samples.R
+++ code/2026-winter-soil-samples.R
@@ -1,5 +1,7 @@
 #script for preliminary visualization of soil moisture and salinity
 
+
+# setup ----
 library(tidyverse)
 library(janitor)
 library(cowplot)
@@ -11,7 +13,7 @@ source("code/mS_per_cm_to_dS_per_m.R")
 
 #read in data, get depth midpoints, and estimate EC in dS/m
 
-soils_2026_winter <- read_csv("data/soil_samples/2026-winter-cores_2026-02-09.csv") %>% 
+soils_2026_winter <- read_csv("data/soil_samples/2026-winter-cores_2026-02-16.csv") %>% 
   clean_names() %>% 
   mutate(subplot = as.factor(subplot),
          depth_midpoint = -(depth_top + depth_bottom)/2,
@@ -27,6 +29,64 @@ ggplot(data = soils_2026_winter, aes(x = ec_converted, y = ec_ds_per_m)) +
   scale_x_continuous(limits = c(0,NA)) +
   scale_y_continuous(limits = c(0, NA))
   
+
+# calculate mean ----
+
+soils_2026_winter_mean <- soils_2026_winter %>% 
+  group_by(depth_midpoint) %>% 
+  summarize(mean_moisture = mean(percent_soil_moisture),
+            mean_conductivity = mean(ec_ds_per_m)) %>% 
+  ungroup()
+
+
+# revised version using coord_flip ----
+#based on this blog: https://rdoodles.rbind.io/2018/09/a-simple-ggplot-of-some-measure-against-depth/#:~:text=Second%20%E2%80%93%20make%20ggplot,TAGGED%20IN
+
+fig_moisture_flip <- ggplot(data = soils_2026_winter, aes(x = depth_midpoint, y = percent_soil_moisture, color = subplot)) +
+  geom_point() +
+  geom_path() +
+  geom_path(data = soils_2026_winter_mean, aes(x = depth_midpoint, y = mean_moisture), color = "black", linewidth = 2) +
+  xlab("Sample depth (cm)") +
+  ylab("% moisture") +
+  coord_flip() +
+  theme_cowplot() +
+  scale_x_continuous(limits = c(NA,0)) +
+  scale_y_continuous(
+    limits = c(0,NA), 
+                     position = "right") +
+  labs(title = "NCOS Mesa West Plot, Jan-Feb 2026")
+
+fig_moisture_flip
+
+fig_EC_flip <- ggplot(data = soils_2026_winter, aes(x = depth_midpoint, y = ec_ds_per_m, color = subplot)) +
+  geom_point() +
+  geom_path() +
+  
+  geom_path(data = soils_2026_winter_mean, aes(x = depth_midpoint, y = mean_conductivity), color = "black", linewidth = 2) +
+  theme_cowplot() +
+  coord_flip() +
+  xlab("Sample depth (cm)") +
+  ylab("Estimated conductivity (dS/m)") +
+  scale_x_continuous(limits = c(NA,0)) +
+  scale_y_continuous(limits = c(0,NA), position = "right") +
+  labs(title = " ")
+  
+fig_EC_flip
+
+#assemble 2-panel figure
+fig_west_plot_winter_2026 <- plot_grid(fig_moisture_flip + theme(legend.position = "none"), 
+                                       fig_EC_flip + theme(legend.position = c(0.6, 0.8)),
+                                   nrow = 1)
+
+fig_west_plot_winter_2026
+
+ggsave(plot = fig_west_plot_winter_2026, filename = "figures/Winter_2026_West_Plot_depth_profiles_DRAFT_2026-02-16.pdf")
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~ ----
+# old code ----
+
 # first draft ----
 
 fig_moisture <- ggplot(data = soils_2026_winter, aes(x = depth, y = percent_soil_moisture, color= subplot)) +
@@ -83,52 +143,10 @@ fig_conductivity_profile
 #assemble 2-panel figure
 
 fig_WP_winter_profile <- plot_grid(fig_moisture_profile, fig_conductivity_profile,
-                           nrow = 1)
+                                   nrow = 1)
 
 fig_WP_winter_profile
 
 #ggsave(plot = fig_WP_winter_profile, filename = "figures/Winter_2026_West_Plot_depth_profiles_DRAFT.pdf")
 
-
-
-
-# third draft- alternate version using coord_flip ----
-#based on this blog: https://rdoodles.rbind.io/2018/09/a-simple-ggplot-of-some-measure-against-depth/#:~:text=Second%20%E2%80%93%20make%20ggplot,TAGGED%20IN
-
-fig_moisture_flip <- ggplot(data = soils_2026_winter, aes(x = depth_midpoint, y = percent_soil_moisture, color = subplot)) +
-  geom_point() +
-  geom_path() +
-  xlab("Sample depth (cm)") +
-  ylab("% moisture") +
-  coord_flip() +
-  theme_cowplot() +
-  scale_x_continuous(limits = c(NA,0)) +
-  scale_y_continuous(
-    limits = c(0,NA), 
-                     position = "right") +
-  labs(title = "NCOS Mesa West Plot, Jan-Feb 2026")
-
-fig_moisture_flip
-
-fig_EC_flip <- ggplot(data = soils_2026_winter, aes(x = depth_midpoint, y = ec_ds_per_m, color = subplot)) +
-  geom_point() +
-  geom_path() +
-  theme_cowplot() +
-  coord_flip() +
-  xlab("Sample depth (cm)") +
-  ylab("Estimated conductivity (dS/m)") +
-  scale_x_continuous(limits = c(NA,0)) +
-  scale_y_continuous(limits = c(0,NA), position = "right") +
-  labs(title = " ")
-  
-fig_EC_flip
-
-#assemble 2-panel figure
-fig_west_plot_winter_2026 <- plot_grid(fig_moisture_flip + theme(legend.position = "none"), 
-                                       fig_EC_flip + theme(legend.position = c(0.6, 0.8)),
-                                   nrow = 1)
-
-fig_west_plot_winter_2026
-
-ggsave(plot = fig_west_plot_winter_2026, filename = "figures/Winter_2026_West_Plot_depth_profiles_DRAFT_2026-02-09.pdf")
 
