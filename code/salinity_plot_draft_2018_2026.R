8aa61453c84fc7b963876900f870bd79a30a3532
fjoyce <fhjoyce@ucsb.edu>
2026-03-02 17:02:36 -0800

    scratchwork script for visualizing 2018 and 2026 EC data on same graph...

---
+++ code/salinity_plot_draft_2018_2026.R
@@ -0,0 +1,43 @@
+#use soils_2018 and soils_2026_winter as inputs
+
+#select subset of columns that match with 2026 data and do some renaming
+ec_2018 <- soils_2018 %>% 
+  mutate(year = year(sample_date)) %>% 
+  select(c(plot, year, subplot_number, depth_midpoint, ec_ds_m)) %>% 
+  rename(subplot = subplot_number)
+
+ec_2026 <- soils_2026_winter %>% 
+  select(plot, year, subplot, depth_midpoint, ec_ds_per_m) %>% 
+  rename(ec_ds_m = ec_ds_per_m)
+
+ec_combined <- bind_rows(ec_2018, ec_2026) %>% 
+  mutate(year = as.factor(year))
+
+ec_combined_mean <- ec_combined %>%
+  group_by(plot, year, depth_midpoint) %>% 
+  summarize(mean_ec = mean(ec_ds_m, na.rm = TRUE),
+            n = n()) %>% 
+  ungroup() 
+  
+fig_ec_combined <- ggplot(data = ec_combined, aes(x = depth_midpoint, y = ec_ds_m, group = subplot)) +
+  geom_path(aes(color = year)) +
+  geom_path(data = ec_combined_mean, aes(x = depth_midpoint, y = mean_ec, group = year, color = year), linewidth = 2) +
+  #geom_point() +
+  #geom_path() +
+  xlab("Sample depth (cm)") +
+  ylab("EC") +
+  coord_flip() +
+  theme_cowplot() +
+  facet_wrap(facets = vars(plot)) +
+  scale_x_continuous(limits = c(-90,0), breaks = seq(-90, 0, by =15)) +
+  scale_y_continuous(limits = c(0,NA), position = "left") +
+  labs(y = "Estimated EC (dS/m)", x = "Depth (cm)", title = "2018 & 2026") +
+  scale_color_ptol()
+
+fig_ec_combined 
+
+#ggplotly(fig_ec_combined)
+
+
+
+  
