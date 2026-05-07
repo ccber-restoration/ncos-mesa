e2d2ec2fdc7f882c6a48423d99f5c3533396cd56
fjoyce <fhjoyce@ucsb.edu>
2026-05-07 10:45:52 -0700

    revisions for SERCAL presentation

--- code/salinity_plot_draft_2018_2026.R
+++ code/salinity_plot_draft_2018_2026.R
@@ -27,7 +27,7 @@ ec_combined_mean <- ec_combined %>%
 
   
 fig_ec_combined <- ggplot(data = ec_combined, aes(x = depth_midpoint, y = ec_ds_m, group = subplot)) +
-  geom_path(aes(color = year), alpha = 0.5) +
+  #geom_path(aes(color = year), alpha = 0.5) +
   geom_path(data = ec_combined_mean, aes(x = depth_midpoint, y = mean_ec, group = year, color = year), linewidth = 2) +
   #geom_point() +
   #geom_path() +
@@ -38,14 +38,53 @@ fig_ec_combined <- ggplot(data = ec_combined, aes(x = depth_midpoint, y = ec_ds_
   facet_wrap(facets = vars(plot)) +
   scale_x_continuous(limits = c(-90,0), breaks = seq(-90, 0, by =15)) +
   scale_y_continuous(limits = c(0,NA), position = "left") +
-  labs(y = "Estimated EC (dS/m)", x = "Depth (cm)", title = "2018 & 2026") +
+  labs(y = "EC (dS/m)", x = "Depth (cm)", title = "2018 & 2026") +
   scale_color_ptol()
 
 fig_ec_combined 
 
 #ggplotly(fig_ec_combined)
 
-ggsave("figures/soil_cores/2018_2026_comparison_lines.png", fig_ec_combined)
+ggsave("figures/soil_cores/sercal/2018_2026_comparison_lines.png", fig_ec_combined,
+       width = 7.46,
+       height = 5.45,
+       units = "in",
+       bg = "white")
+
+# for SERCAL, just Central plot
+
+ec_combined_cp <- ec_combined %>% 
+  filter(plot == "Central Plot")
+
+ec_combined_mean_cp <- ec_combined_cp %>%
+  group_by(plot, year, depth_midpoint) %>% 
+  summarize(mean_ec = mean(ec_ds_m, na.rm = TRUE),
+            n = n()) %>% 
+  ungroup()
+
+
+fig_ec_combined_cp <- ggplot(data = ec_combined_cp, aes(x = depth_midpoint, y = ec_ds_m, group = subplot)) +
+  #geom_path(aes(color = year), alpha = 0.5) +
+  geom_path(data = ec_combined_mean_cp, aes(x = depth_midpoint, y = mean_ec, group = year, color = year), linewidth = 2) +
+  #geom_point() +
+  #geom_path() +
+  xlab("Sample depth (cm)") +
+  ylab("EC") +
+  coord_flip() +
+  theme_cowplot() +
+  #facet_wrap(facets = vars(plot)) +
+  scale_x_continuous(limits = c(-90,0), breaks = seq(-90, 0, by =15)) +
+  scale_y_continuous(limits = c(0,NA), position = "left") +
+  labs(y = "EC (dS/m)", x = "Depth (cm)", title = "Central plot: 2018 vs. 2026") +
+  scale_color_ptol()
+
+fig_ec_combined_cp 
+
+ggsave("figures/soil_cores/sercal/Central_Plot_EC_2018_2026_comparison_lines.png", fig_ec_combined,
+       width = 7.46,
+       height = 5.45,
+       units = "in",
+       bg = "white")
 
 
 
